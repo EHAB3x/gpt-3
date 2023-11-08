6ff1b4cd0dd7e382e6d1ee8d6ab7296394d7a9c6
EHAB3x <ehabmah6969@gmail.com>
2023-11-08 06:24:57 +0200

    structure project folders and components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import { Cta , Brand , Navbar} from './components/index'
+import {Blog, Features, Footer, Header, Possibility, WhatGPT3} from './containers/index'
 const App = () => {
   return (
-    <div>
-        <h1>GPT-3</h1>
+    <div className='App'>
+        <div className='gradient_bg'>
+        <Navbar />
+        <Header />
+        </div> 
+        <Brand />
+        <WhatGPT3 />
+        <Features />
+        <Possibility />
+        <Cta />
+        <Blog />
+        <Footer/>
     </div>
   )
 }
---
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './whatGPT.css'
+const WhatGPT3 = () => {
+  return (
+    <div>WhatGPT3</div>
+  )
+}
+
+export default WhatGPT3
